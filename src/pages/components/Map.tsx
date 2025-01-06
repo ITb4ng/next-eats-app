9b69a401d4028ee76479c85aef58f7c7edc5f71b
itb4ng <bsh801099@gmail.com>
2025-01-06 10:13:14 +0900

    데이터 추출

--- src/pages/components/Map.tsx
+++ src/pages/components/Map.tsx
@@ -1,4 +1,9 @@
 import Script from "next/script";
+//서울 공공 데이터 import * as stores from '@/data/store_data.json';
+
+//춘천 공공 데이터 import * as stores from '@/data/store_data_upso.json';
+import * as stores from '@/data/test.json';
+// import { Dispatch, SetStateAction } from "react";
 
 declare global {
     interface Window {
@@ -6,16 +11,36 @@ declare global {
     }
 }
 
+// 우리 집const DEFAULT_LAT = 37.8689472;
+// const DEFAULT_LNG = 127.72065;
+const DEFAULT_LAT = 37.64308901450529; 
+const DEFAULT_LNG = 127.3281682502683;
+
 export default function Map() {
     const loadkakaomap = () => {
         //Kakao map laod
         window.kakao.maps.load(function() {
           const mapContainer = document.getElementById("map");
           const mapOption = {
-            center: new window.kakao.maps.LatLng(37.8689472, 127.72065),
-            level: 3,
+            center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+            level: 9,
           };
-          new window.kakao.maps.Map(mapContainer, mapOption);
+          const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+          stores?.["DATA"]?.map((store) =>{
+            var makerPosition = new window.kakao.maps.LatLng(
+              store?.y_dnts,
+              store?.x_cnts
+            );
+
+            //마커 생성
+
+            var marker = new window.kakao.maps.Marker({
+              position: makerPosition,
+            });
+            
+            marker.setMap(map);
+          });
         });
       };
     return (
